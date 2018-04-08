56b81f92ab99dd4e93fac3180d9a20d2901e5f46
wpolis <wpolis@gmail.com>
2018-04-08 15:55:57 -0300

    Binary Search Tree

--- data-structure-js/6 - BST.js
+++ data-structure-js/6 - BST.js
@@ -15,6 +15,47 @@ BST.prototype.insert = function(value) {
   
 };
 
+BST.prototype.contains = function(value) {
+  if (value === this.value) return true;
+  else if (value < this.value) {
+    if (!this.left) return false;
+    else return this.left.contains(value);
+  } else if (value > this.value) {
+    if (!this.right) return false;
+    else return this.right.contains(value);
+  }
+};
+
+BST.prototype.depthFirstTraversal = function(iteratorFunc, order) {
+  if (order === 'pre-order') iteratorFunc(this);
+  if (this.left) this.left.depthFirstTraversal(iteratorFunc, order);
+  if (order === 'in-order') iteratorFunc(this);
+  if (this.right) this.right.depthFirstTraversal(iteratorFunc, order);
+  if (order === 'post-order') iteratorFunc(this)
+};
+
+BST.prototype.breadthFirstTraversal = function(iteratorFunc) {
+  var queue = [this];
+  while (queue.length) {
+    var treeNode = queue.shift();
+    iteratorFunc(treeNode);
+    if (treeNode.left) queue.push(treeNode.left);
+    if (treeNode.right) queue.push(treeNode.right);
+  }
+  
+};
+
+
+BST.prototype.getMinVal = function() {
+  if (this.left) return  this.left.getMinVal();
+  else return this.value;
+};
+
+BST.prototype.getMaxVal = function() {
+  if (this.right) return  this.right.getMaxVal();
+  else return this.value;
+};
+
 var bst = new BST(50);
 bst.insert(30);
 bst.insert(70);
@@ -28,4 +69,23 @@ bst.insert(85);
 bst.insert(105);
 bst.insert(10);
 
-console.log(bst.right.right);
+console.log('---- in-order');
+bst.depthFirstTraversal(log, 'in-order');
+console.log('---- pre-order');
+bst.depthFirstTraversal(log, 'pre-order');
+console.log('---- post-order');
+bst.depthFirstTraversal(log, 'post-order');
+console.log('---- breadthFirstTraversal');
+bst.breadthFirstTraversal(log);
+
+function log(node) {
+  console.log(node.value);
+}
+
+console.log(bst.contains(1000));
+
+console.log('---- getMinVal');
+console.log(bst.getMinVal());
+
+console.log('---- getMaxVal');
+console.log(bst.getMaxVal());
