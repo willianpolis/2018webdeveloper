bc5c96680715c8bfd58118ef76364bbce82de632
wpolis <wpolis@gmail.com>
2018-04-08 13:37:28 -0300

    Reorder files name

---
+++ data-structure-js/1 - Constructor.js
@@ -0,0 +1,13 @@
+// Constructor
+function User(firstName, lastName, age, gender) {
+	this.firstName = firstName;
+	this.lastName = lastName;
+	this.age = age;
+	this.gender = gender;
+};
+
+var user1 = new User('John', 'Smith', 26, 'male');
+user1
+
+var user200 = new User('Jill','Robinson', 25,'female');
+user200
